882640f229838206cdd2562794a46457cc3cb6ed
Melika <melika.411@gmail.com>
2024-04-22 16:32:46 +0200

    Fix 403 error, save the data to use.

--- API.py
+++ API.py
@@ -1,17 +1,34 @@
 import requests
+import json
 
-def get_restaurants_data(postcode):
+
+err = "unable to fetch restaurant data"
+
+
+def get_restaurant_data(postcode):
     url = f"https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/{postcode}"
+    #check why this header works
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
 
-    response = requests.get(url)
-    response_type = response.headers.get('content_type', 'unknown')
-    print(f"response type: {response_type}")
+    response = requests.get(url, headers=headers)
 
-    print(response.content.decode('utf-8'))
-    return None
+    if response.status_code == 200:
+        print("API fetched")
+        return response.json()
+    else:
+        print(err)
+        return None
 
+def save_json(data, filename):
+    with open(filename, 'w') as j_file:
+        json.dump(data, j_file, indent=4)
 
-#test
-postcode = "EC4M7RF"
-restaurants_data = get_restaurants_data(postcode)
+if __name__ == "__main__":
+    postcode = input("Enter postcode: ")
+    restaurant_data = get_restaurant_data(postcode)
 
+    if restaurant_data:
+        save_json(restaurant_data, f'restaurant_data{postcode}.json')
+    else:
+        print(err)
