36a05e4b159920e9a090621af41023a059c0f051
Melika <melika.411@gmail.com>
2024-04-22 17:09:19 +0200

    Read the json file and save it to a csv

---
+++ read_json.py
@@ -0,0 +1,39 @@
+import json
+import csv
+
+def write_to_csv(file_path, rest_list):
+    with open(file_path, 'w', newline='', encoding='utf-8') as csv_file:
+        headers = ['Name', 'Cuisines', 'Rating', 'Address']
+        writer = csv.DictWriter(csv_file, fieldnames=headers)
+        writer.writeheader()
+        for rest in rest_list:
+            cuisines = ", ".join([cuisine.get('name', '') for cuisine in rest.get('cuisines', [])])
+            writer.writerow({
+                'Name': rest.get('name', ''),
+                'Cuisines': cuisines,
+                'Rating': rest.get('rating', {}).get('starRating', ''),
+                'Address': f"{rest.get('address', {}).get('firstLine', '')}, {rest.get('address', {}).get('city', '')}, {rest.get('address', {}).get('postalCode', '')}"
+            })
+
+def get_data(file_path):
+    with open(file_path, 'r') as j_file:
+        data = json.load(j_file)
+        if 'restaurants' in data:
+            rest = data['restaurants'][:10]
+            return rest
+        else:
+            return None
+
+
+if __name__ == "__main__":
+    file_path = '/Users/melikadavoodzade/Documents/GitHub/datamining-assigment/DM/JET/restaurant_dataNE97TY.json'  # Replace 'restaurant_data.json' with the path to your JSON file
+    restaurants = get_data(file_path)
+    if restaurants:
+        print("Restaurant data retrieved successfully!")
+
+        csv_file_path = f'restaurants.csv'  # Specify the path for the CSV file
+        write_to_csv(csv_file_path, restaurants)
+        print("Restaurant data written to CSV file:", csv_file_path)
+    else:
+        print("Error: No 'restaurants' data found in the JSON file.")
+
